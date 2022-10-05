b2d5f401bf0070fd1732acdfc75519454f3c1632
Sergi Juanati <wint3rmute@gmail.com>
2022-10-05 16:15:29 +0200

    Yet types on personalStatsTotals

--- src/parser/personalStatsTotals.ts
+++ src/parser/personalStatsTotals.ts
@@ -1,64 +1,74 @@
+import moment from 'moment';
 import { showError } from '../handler/logHandler';
+import {
+    IPersonalStatsEthereum,
+    IPersonalStatsAvalanche,
+    IPersonalStatsTotals,
+} from '../interfaces/IPersonalStats';
+import { Status } from '../types';
+import { personalStatsError } from './personalStatsError';
 
 export const personalStatsSubgraphParserTotals = (
-    stats_eth: any,
-    stats_avax: any,
-) => {
+    stats_eth: IPersonalStatsEthereum,
+    stats_avax: IPersonalStatsAvalanche,
+): IPersonalStatsTotals => {
     try {
         const personalStats = {
             "gro_personal_position_mc": {
                 "status": (
-                    stats_eth.ethereum.status === 'ok'
-                    && stats_avax.avalanche.status === 'ok')
-                    ? 'ok'
-                    : 'error',
-                "current_timestamp": stats_eth.ethereum.current_timestamp,
-                "address": stats_eth.ethereum.address,
-                "network": stats_eth.ethereum.network,
+                    stats_eth.status === 'ok'
+                    && stats_avax.status === 'ok')
+                    ? Status.ok
+                    : Status.error,
+                "current_timestamp": stats_eth.current_timestamp,
+                "address": stats_eth.address,
+                "network": stats_eth.network,
                 "mc_totals": {
                     "amount_added": {
-                        "ethereum": stats_eth.ethereum.amount_added.total,
-                        "avalanche": stats_avax.avalanche.amount_added.total,
+                        "ethereum": stats_eth.amount_added.total,
+                        "avalanche": stats_avax.amount_added.total,
                         "total": (
-                            parseFloat(stats_eth.ethereum.amount_added.total)
-                            + parseFloat(stats_avax.avalanche.amount_added.total)
+                            parseFloat(stats_eth.amount_added.total)
+                            + parseFloat(stats_avax.amount_added.total)
                         ).toString()
                     },
                     "amount_removed": {
-                        "ethereum": stats_eth.ethereum.amount_removed.total,
-                        "avalanche": stats_avax.avalanche.amount_removed.total,
+                        "ethereum": stats_eth.amount_removed.total,
+                        "avalanche": stats_avax.amount_removed.total,
                         "total": (
-                            parseFloat(stats_eth.ethereum.amount_removed.total)
-                            + parseFloat(stats_avax.avalanche.amount_removed.total)
+                            parseFloat(stats_eth.amount_removed.total)
+                            + parseFloat(stats_avax.amount_removed.total)
                         ).toString()
                     },
                     "net_amount_added": {
-                        "ethereum": stats_eth.ethereum.net_amount_added.total,
-                        "avalanche": stats_avax.avalanche.net_amount_added.total,
+                        "ethereum": stats_eth.net_amount_added.total,
+                        "avalanche": stats_avax.net_amount_added.total,
                         "total": (
-                            parseFloat(stats_eth.ethereum.net_amount_added.total)
-                            + parseFloat(stats_avax.avalanche.net_amount_added.total)
+                            parseFloat(stats_eth.net_amount_added.total)
+                            + parseFloat(stats_avax.net_amount_added.total)
                         ).toString()
                     },
                     "current_balance": {
-                        "ethereum": stats_eth.ethereum.current_balance.total,
-                        "avalanche": stats_avax.avalanche.current_balance.total,
+                        "ethereum": stats_eth.current_balance.total,
+                        "avalanche": stats_avax.current_balance.total,
                         "total": (
-                            parseFloat(stats_eth.ethereum.current_balance.total)
-                            + parseFloat(stats_avax.avalanche.current_balance.total)
+                            parseFloat(stats_eth.current_balance.total)
+                            + parseFloat(stats_avax.current_balance.total)
                         ).toString()
                     },
                     "net_returns": {
-                        "ethereum": stats_eth.ethereum.net_returns.total,
-                        "avalanche": stats_avax.avalanche.net_returns.total,
+                        "ethereum": stats_eth.net_returns.total,
+                        "avalanche": stats_avax.net_returns.total,
                         "total": (
-                            parseFloat(stats_eth.ethereum.net_returns.total)
-                            + parseFloat(stats_avax.avalanche.net_returns.total)
+                            parseFloat(stats_eth.net_returns.total)
+                            + parseFloat(stats_avax.net_returns.total)
                         ).toString()
                     },
                 },
-                ...stats_eth,
-                ...stats_avax
+                // ...stats_eth,
+                // ...stats_avax
+                'ethereum': stats_eth,
+                'avalanche': stats_avax,
             }
         }
         return personalStats;
@@ -67,6 +77,9 @@ export const personalStatsSubgraphParserTotals = (
             'personalStatsSubgraphParserTotals.ts->personalStatsSubgraphParserTotals()',
             `${err}`,
         );
-        //TODO: return error in json format
+        return personalStatsError(
+            moment().unix().toString(),
+            'test'
+        )
     }
 }
