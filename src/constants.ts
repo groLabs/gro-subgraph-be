9832eaffefcac014d8da9b0acbc4f3cd9b977173
Sergi Juanati <wint3rmute@gmail.com>
2023-02-19 16:35:32 +0100

    Preset Pwrd apy to constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { Token } from './types';
 
 
+export const PWRD_APY = 0.02;
 export const QUERY_ERROR = 400;
 export const QUERY_SUCCESS = 200;
 export const NA = 'N/A';
@@ -8,9 +9,9 @@ export const TX_ITERATION = 999; // # of records retrieved per subgraph call (li
 export const MAX_VEST_TIME = 31556952;
 export const LAUNCH_TIMESTAMP_ETH = '1622204347';
 export const LAUNCH_TIMESTAMP_AVAX = '1638483222';
-export const TS_1D = 86400;             // One day
-export const TS_7D = 604800;            // Seven days
-export const TS_15D = 1296000;          // Fifteen days
+export const TS_1D = 86400;     // one day
+export const TS_7D = 604800;    // seven days
+export const TS_15D = 1296000;  // fifteen days
 export const DECIMALS = 7;
 export const BLOCKS_PER_YEAR = 2252571;
 export const PWRD_TVL_CORRECTION = 432.5559;  // correction due to rebasing (as of Nov'22)
@@ -25,12 +26,14 @@ export const STABLECOINS = [
     Token.GUSD,
 ];
 export const DEFAULT_STRATEGY_APY = new Map<string, number>([
+    ['0x02ba6fa63e094499a03f76bb7f19d191c0f9742a', 0.05],  // FRAX
+    ['0xaeeed92f98e3362c49b2111388715354bf838c03', 0.03],  // MIM
+]);
+
+// Gro protocol strategies
     // ['0xdea436e15b40e7b707a7002a749f416dfe5b383f', 0.05],   // FRAX
     // ['0xd1b9af64ed5cdcaeb58955d82fb384b3e558df7b', 0.02],   // FRAX
     // ['0x4d5b5376cbcc001bb4f8930208828ab87d121da8', 0.04],   // mUSD->TUSD
     // ['0xd370998b2e7941151e7bb9f6e337a12f337d0682', 0.023],  // gUSD
     // ['0x8b335d3e266389ae08a2f22b01d33813d40ed8fd', 0.09],   // alUSD
     // ['0xde5a25415c637b52d59ef980b29a5fda8dc3c70b', 0.06],   // TUSD->oUSD
-    ['0x02ba6fa63e094499a03f76bb7f19d191c0f9742a', 0.05],  // FRAX
-    ['0xaeeed92f98e3362c49b2111388715354bf838c03', 0.03],  // MIM
-]);
--- src/utils/apy.ts
+++ src/utils/apy.ts
@@ -1,13 +1,14 @@
 import { toStr } from './utils';
+import { PWRD_APY } from '../constants';
 
 
-// avoid division/s by 0
+// avoid division by 0
 const rekt = {
     'pwrd': toStr(0),
     'gvt': toStr(0),
 }
 
-// @dev: Pwrd APY set to fixed 2% with G^2
+// @dev: preset pwrd apy with G^2, other gain/loss goes to gvt   
 export const getCoreApy = (
     tvlGvt: number,
     tvlPwrd: number,
@@ -15,12 +16,10 @@ export const getCoreApy = (
 ) => {
     const u = (tvlGvt > 0) ? tvlPwrd / tvlGvt : 0;
     if (u === 0)
-        return rekt;
-    let pwrd = 0.02;
-    // Keep pwrd 2% apy, other gain/loss will go to gvt    
-    let gvt = trancheApy * (1 + u) - (pwrd * u)
+        return rekt; 
+    const gvt = trancheApy * (1 + u) - (PWRD_APY * u)
     return {
-        'pwrd': toStr(pwrd),
+        'pwrd': toStr(PWRD_APY),
         'gvt': toStr(gvt),
     }
 }
