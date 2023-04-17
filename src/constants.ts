82d8c1df72e6459b169645d9a6622d0ae6607058
Sergi Juanati <wint3rmute@gmail.com>
2023-04-17 11:07:24 +0200

    Added new strategies

--- src/constants.ts
+++ src/constants.ts
@@ -24,6 +24,20 @@ export const UST_VESTING_AIRDROP = {
 
 // Post-G2 strategies: average of the estimated APY from the Convex pools as of 27.02.23
 export const DEFAULT_STRATEGY_APY = new Map<string, number>([
+    ['0x60a6a86ad77ef672d93db4408d65cf27dd627050', 0.0308],  // FRAX
+    ['0x4d81d0c2655d8d5fdee83dbb16e6b899ec276fac', 0.0019],  // LUSD
+    ['0x73703f0493c08ba592ab1e321bead695ac5b39e3', 0.00023],  // OUSD
+    ['0xa522b13fef6161c570ff765c986cb9992a89c786', 0.0352],  // TUSD
+]);
+export const DEFAULT_AVERAGE_STRATEGY_APY = 
+      0.0308 * 0.25     // FRAX
+    + 0.0019 * 0.25     // LUSD
+    + 0.00023 * 0.25     // OUSD
+    + 0.0352 * 0.25;    // TUSD
+
+// G2 strategies v1
+/*
+export const DEFAULT_STRATEGY_APY = new Map<string, number>([
     ['0xd18415e9bc188f113cb54a9edd86df21898555c7', 0.0293],  // FRAX
     ['0x708d0089d52d57e911024465e841774634466608', 0.0174],  // LUSD
     ['0xa1327c0cd1e04e82fd99e68b46ab8a6eb15b17ae', 0.0390],  // GUSD
@@ -31,11 +45,12 @@ export const DEFAULT_STRATEGY_APY = new Map<string, number>([
     ['0xd849d8551ec988a59d4e411b1ed7b5b40bf97159', 0.0382],  // TUSD
 ]);
 export const DEFAULT_AVERAGE_STRATEGY_APY = 
-    0.0293 * 0.25       // FRAX
+      0.0293 * 0.25       // FRAX
     + 0.0174 * 0.12     // LUSD
     + 0.0390 * 0.13     // GUSD
     + 0.0221 * 0.25     // OUSD
     + 0.0382 * 0.25;    // TUSD
+*/
 
 // Pre-G2 strategies
 /*
